69128c120f020920f7139c32b578cb38b0555f9a
Kajal Kumari <kajaldob06@gmail.com>
2026-09-15 14:14:22 +0530

    theme switcher

---
+++ mini-context2/src/contexts/ContextProvider.jsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+import { ThemeProvider } from "./theme";
+
+export default function ContextProvider({ children }) {
+
+    const [themeMode, setThemeMode] = useState("light");
+
+    const lightTheme = () => {
+        setThemeMode("light");
+    };
+
+    const darkTheme = () => {
+        setThemeMode("dark");
+    };
+
+    useEffect(() => {
+        document.querySelector("html").classList.remove("light", "dark");
+        document.querySelector("html").classList.add(themeMode);
+    }, [themeMode]);
+
+    return (
+        <ThemeProvider value={{ themeMode, lightTheme, darkTheme }}>
+            {children}
+        </ThemeProvider>
+    );
+}
