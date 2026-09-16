60546d2cd763eec425cc1f4fffe28c335d69f33f
Kajal Kumari <kajaldob06@gmail.com>
2026-09-16 20:23:48 +0530

    Todo with context and local storage

--- mini-context2/src/contexts/ContextProvider.jsx
+++ mini-context2/src/contexts/ContextProvider.jsx
@@ -13,14 +13,20 @@ export default function ContextProvider({ children }) {
         setThemeMode("dark");
     };
 
+
+    // useEffect side effect handle karta hai. Mere project me jab themeMode change hota hai,
+    //  tab useEffect <html> element ki class ko light ya dark karta hai.
     useEffect(() => {
-        document.querySelector("html").classList.remove("light", "dark");
+        document.querySelector("html").classList.remove("light", "dark");//Browser ke poore HTML document ko represent karta hai.
         document.querySelector("html").classList.add(themeMode);
-    }, [themeMode]);
+    }, [themeMode]); //[themeMode] ke wajah se themeMode change hone par useEffect dobara execute hota hai.
 
     return (
-        <ThemeProvider value={{ themeMode, lightTheme, darkTheme }}>
+        <ThemeProvider value={{ themeMode, lightTheme, darkTheme }}> //ThemeProvider Context ke through themeMode, 
+        {/* lightTheme aur darkTheme ko child components tak pahunchata hai. */}
             {children}
+            {/* ContextProvider ke andar jo bhi component/content likha hota hai, wahi children hota hai. */}
+{/* //{children} isliye likha hai taaki Provider ke andar jo component diya gaya hai, wo render ho aur Context ka data us component ko mil sake. */}
         </ThemeProvider>
     );
 }
